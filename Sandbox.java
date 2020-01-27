085cbd95d5526325230a55cddb141463ded908cd
Radeef <radeefchowdhury@gmail.com>
2020-01-27 11:17:31 -0500

    sandbox

---
+++ Sandbox.java
@@ -0,0 +1,18 @@
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World,this is my Sandbox");
+
+        final double PI = 3.14159;
+
+        double radius = 3.5;
+        double area; 
+
+        area = PI * radius * radius;
+
+        System.out.println("\nThe area of the circle is" + area);
+        
+    }
+
+}
